5aa4d2e7d64fda9a547298082161aa3018f4bc70
khoata2025 <khoata2025@gmail.com>
2025-09-15 08:31:04 +0700

    offline2

--- qlvt.py
+++ qlvt.py
@@ -209,7 +209,7 @@ class WordProcessorApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.current_version = "1.0.20"
+        self.current_version = "1.0.21"
         
         # Khởi tạo progress bar
         self.progress_bar = None
